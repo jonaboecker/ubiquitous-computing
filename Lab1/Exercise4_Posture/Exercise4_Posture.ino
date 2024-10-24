bc03ffe151cd38a8b26a9775cf57342a095c54af
ThatsNotMe <mineflax@gmail.com>
2024-10-24 09:12:03 +0200

    finished excercise 4

--- Lab1/Exercise4_Posture/Exercise4_Posture.ino
+++ Lab1/Exercise4_Posture/Exercise4_Posture.ino
@@ -1,5 +1,6 @@
 #include <Arduino_LSM6DSOX.h>
 #include <MadgwickAHRS.h>
+#include <WiFiNINA.h>
 
 Madgwick filter;
 unsigned long microsPerReading, microsPrevious;
@@ -7,19 +8,24 @@ float accelScale, gyroScale;
 
 void setup() {
   Serial.begin(9600);
+  pinMode(LEDB, OUTPUT);
 
   // start the IMU and filter
   if (!IMU.begin()) {
       Serial.println("Failed to initialize IMU!");
       while (1);
   }
-  filter.begin(25);
+  filter.begin(104);
 }
 
 void loop() {
   float ax, ay, az;
   float gx, gy, gz;
   float roll, pitch, heading;
+
+  int temp_deg = 0;
+  IMU.readTemperature(temp_deg);
+  temp_deg -= 12;
 
   // read raw data from IMU
   IMU.readAcceleration(ax, ay, az);
@@ -29,14 +35,27 @@ void loop() {
   filter.updateIMU(gx, gy, gz, ax, ay, az);
 
   // print the heading, pitch and roll
-  roll = filter.getRoll();
+  //roll = filter.getRoll();
   pitch = filter.getPitch();
-  heading = filter.getYaw();
+  //heading = filter.getYaw();
   Serial.print("Orientation: ");
-  Serial.print(heading);
-  Serial.print(" ");
+  //Serial.print(heading);
+  //Serial.print(" ");
   Serial.print(pitch);
-  Serial.print(" ");
-  Serial.println(roll);
-  
+
+  Serial.print("\t Temperatur: ");
+  Serial.print(temp_deg);
+  Serial.print("\n");
+
+  //Serial.print(" ");
+  //Serial.println(roll);
+
+  if (pitch < 80 || temp_deg > 26 || temp_deg < 18) {
+    digitalWrite(LEDB, HIGH);
+  } else {
+    digitalWrite(LEDB, LOW);
+  }
+
+  delay(5);
+
 }
